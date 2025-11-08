60c5200f487e574530e1ec6311eb68584be0f139
mustafagoktugibolar <mustafagoktugibolar@gmail.com>
2025-11-08 19:55:31 +0300

    FastAPI configuration and probe implemented

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,11 @@
-print("Hello, music segmentation project!")
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+  
+@app.get("/probe")
+def probe():
+    return {"status": "app running!"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
